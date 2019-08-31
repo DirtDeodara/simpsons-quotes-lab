8776bacd892d73d66087eb08a6c24beaf44cb7f6
Dirt Deodara <thecrowleaps@gmail.com>
2019-08-31 13:13:15 -0700

    updated the api call

--- src/services/simpsonsApi.js
+++ src/services/simpsonsApi.js
@@ -1,7 +1,10 @@
-export const getQuote = () => {
-  return fetch('https://thesimpsonsquoteapi.glitch.me/quotes')
+export const getSimpsonsQuote = () => {
+  return fetch('https://thesimpsonsquoteapi.glitch.me/quotes?count=1')
     .then(res => {
-      if(!res.ok) throw 'Cannot deliver the wit you so desire';
+      if(!res.ok) throw 'Sadly, I cannot deliver the wit and beauty you so desire.';
       return res.json();
+    })
+    .then(json => {
+      return (json[0]);
     });
 };
