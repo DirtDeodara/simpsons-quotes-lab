5f435646372d15e91377f10e1d5fd5db2a79bb1a
Dirt Deodara <thecrowleaps@gmail.com>
2019-08-31 13:15:23 -0700

    brought quotecontainer into App

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
+import QuoteContainer from '../containers/facts/QuoteContainer';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return <QuoteContainer/>;
 }
-  
